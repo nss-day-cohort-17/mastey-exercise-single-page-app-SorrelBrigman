8822202f87d518753fe71abf687373a89cc88e7e
Sorrel Brigman <sorrel.k@gmail.com>
2016-12-11 14:21:09 -0600

    added unique class to each card to target listener event

--- quiz.js
+++ quiz.js
@@ -1,25 +1,27 @@
-// var inventory = [];
-// loadInventory();
+//GLOBAL VARIABLE
+
 var data;
 
-// function populatePage (inventory) {
-//   // Loop over the inventory and populate the page
 
-//   // Now that the DOM is loaded, establish all the event listeners needed
-//   activateEvents();
-// }
 
-// function populatePage (inventory) {
-//   // Loop over the inventory and populate the page
+
+////////////////////////////
+//Functions
+///////////////////////////
+
+
+
+//function to populate the page
 
 var populatePage = function () {
-    console.log(data.cars);
     var cardInfo = "";
   for (var i = 0; i < data.cars.length; i++) {
       var cardHolder = document.querySelector(".cardHolder");
+      //addes unique class to each card for listener event targeting
+      var targetClass = "class='cardNumber" + i + "'";
       //if the counter is at a value divisible by 3, it will start a new row
       if ((i%3) === 0) {
-      cardInfo += `<div class="row"><div class="col-sm-3 card">
+      cardInfo += `<div class="row"><div class="col-sm-3 card ${targetClass}">
           <p>Make: <span class="make">${data.cars[i].make}</span></p>
           <p>Model: <span class="model">${data.cars[i].model}</span></p>
           <p>Year: <span class="year">${data.cars[i].year}</span></p>
@@ -28,8 +30,8 @@ var populatePage = function () {
           <p>Availability: <span class="avail">${data.cars[i].availability}</span></p>
         </div>`;
         // if the card is the 3rd card on the row, or the last car card on the page it will add the closing div for the row
-      } else if ((i%3) === 2 || i === (data.cars.length)) {
-        cardInfo += `<div class="col-sm-3 card">
+      } else if ((i%3) === 2 || i === (data.cars.length - 1)) {
+        cardInfo += `<div class="col-sm-3 card ${targetClass}">
           <p>Make: <span class="make">${data.cars[i].make}</span></p>
           <p>Model: <span class="model">${data.cars[i].model}</span></p>
           <p>Year: <span class="year">${data.cars[i].year}</span></p>
@@ -37,8 +39,8 @@ var populatePage = function () {
           <p>Description: <span class="descr">${data.cars[i].description}</span></p>
           <p>Availability: <span class="avail">${data.cars[i].availability}</span></p>
         </div></div>`;
-        //otherwise it add the  card without adding in row div tags
-      } cardInfo += `<div class="col-sm-3 card">
+        //otherwise it add the card without adding in row div tags
+      }  else { cardInfo += `<div class="col-sm-3 card ${targetClass}">
           <p>Make: <span class="make">${data.cars[i].make}</span></p>
           <p>Model: <span class="model">${data.cars[i].model}</span></p>
           <p>Year: <span class="year">${data.cars[i].year}</span></p>
@@ -46,6 +48,7 @@ var populatePage = function () {
           <p>Description: <span class="descr">${data.cars[i].description}</span></p>
           <p>Availability: <span class="avail">${data.cars[i].availability}</span></p>
         </div>`;
+      }
   }
   // loads card info into the page
           cardHolder.innerHTML = cardInfo;
@@ -69,3 +72,12 @@ function loadInventory () {
 }
 
 loadInventory();
+
+
+//create a function to house all the event listeners
+
+// var activateEvents = function () {
+// //listener to click on card
+// //listener to
+
+// }
